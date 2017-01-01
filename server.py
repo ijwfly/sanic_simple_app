881332d85fe5ed3afda67106580dc85ef599f155
ijwfly <maxnecha@gmail.com>
2017-01-01 16:10:58 +0300

    minor server refactor

--- server.py
+++ server.py
@@ -3,16 +3,24 @@ from sanic import Blueprint
 from sanic.response import html
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('./templates'))
-bp = Blueprint("main")
+class WebServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        template_env = Environment(loader=FileSystemLoader('./templates'))
+        bp = Blueprint("main")
+        self.sanic = Sanic()
 
-@bp.route("/")
-async def test(request):
-    template = env.get_template('home.html')
-    return html(template.render())
+        @bp.route("/")
+        async def home(request):
+            template = template_env.get_template("home.html")
+            return html(template.render())
+            
+        self.sanic.blueprint(bp);
 
-app = Sanic()
-app.blueprint(bp)
+    def run(self):
+        self.sanic.run(host=self.host, port=self.port)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    web_server = WebServer("0.0.0.0", 8000)
+    web_server.run()
